8aaa9de9164bc6d1b87ff6ba158488b7491862b6
Welington Silva Araujo <welington.sa@gmail.com>
2023-09-25 13:10:29 -0300

    Paginas series criada usando mixin pug

--- index.js
+++ index.js
@@ -40,6 +40,54 @@ app.get('/index',(req,res) => {
     });
 })
 
+/** Template teste das paginas series ***** */
+app.get('/thebigbangtheory',(req,res) => {
+    res.render('thebigbangtheory',{
+        bbt_nome : bd_bbt.nome,
+        bbt_fotoPoster : bd_bbt.fotoPoster,
+        bbt_generos: bd_bbt.generos,
+        bbt_num_episodios : bd_bbt.num_episodios,
+        bbt_num_temporadas : bd_bbt.num_temporadas,
+        bbt_nota : bd_bbt.nota,
+        bbt_resumo : bd_bbt.resumo,
+
+        bbt_elenco : bd_bbt.elenco,
+        bbt_temporadas : bd_bbt.temporadas
+    })
+})
+
+/** Stranges Things page */
+app.get('/strangerthings',(req,res) =>{
+    res.render('strangerthings',{
+        st_nome : bd_st.nome,
+        st_fotoPoster : bd_st.fotoPoster,
+        st_generos : bd_st.generos,
+        st_num_episodios : bd_st.num_episodios,
+        st_num_temporadas : bd_st.num_temporadas,
+        st_nota : bd_st.nota,
+        st_resumo : bd_st.resumo,
+        st_elenco : bd_st.elenco,
+        st_temporadas : bd_st.temporadas
+
+    })
+})
+
+app.get('/sobrenatural',(req,res) => {
+    res.render('sobrenatural', {
+        sn_nome : bd_sn.nome,
+        sn_fotoPoster : bd_sn.fotoPoster,
+        sn_generos : bd_sn.generos,
+        sn_num_episodios : bd_sn.num_episodios,
+        sn_num_temporadas : bd_sn.num_temporadas,
+        sn_nota : bd_sn.nota,
+        sn_resumo : bd_sn.resumo,
+        sn_elenco : bd_sn.elenco,
+        sn_temporadas : bd_sn.temporadas
+    })
+})
+
+
+
 app.listen(port, () =>{
     console.log('App running at port ${port}');
 })
